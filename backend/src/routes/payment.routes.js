6cb49b4a42fee493af7f9f3f29a344a20b091ab9
German717 <germanponce2001@gmail.com>
2026-02-20 09:25:04 -0300

    corrección de errores
    Agregar ruta para el Webhook de Mercado Pago en payment.routes.js

--- backend/src/routes/payment.routes.js
+++ backend/src/routes/payment.routes.js
@@ -1,9 +1,13 @@
 const { Router } = require('express');
 const router = Router();
-const { crearPreferencia } = require('../controllers/payment.controller');
+
+// 👇 AQUÍ ESTÁ EL CAMBIO: Agregamos recibirWebhook en la importación
+const { crearPreferencia, recibirWebhook } = require('../controllers/payment.controller');
 
 // Ruta POST que llamará tu Frontend
 router.post('/crear-orden', crearPreferencia);
+
+// Ruta para el Webhook de Mercado Pago
 router.post('/webhook', recibirWebhook);
 
 module.exports = router;
