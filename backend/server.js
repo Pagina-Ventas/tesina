94ff49b883ff04828604cbd147fb36feb14e1235
German717 <germanponce2001@gmail.com>
2026-03-19 16:29:55 -0300

    .
    .

--- backend/server.js
+++ backend/server.js
@@ -4,5 +4,5 @@ const app = require('./src/app');
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log(`🚀 Servidor ImperioMate corriendo en puerto ${PORT}`);
+  console.log(`🚀 Servidor ApoloMate corriendo en puerto ${PORT}`);
 });
--- backend/src/services/mail.service.js
+++ backend/src/services/mail.service.js
@@ -16,14 +16,14 @@ const transporter = nodemailer.createTransport({
 const enviarCorreoCompra = async (emailCliente, nombreCliente, idPedido, total) => {
     try {
         const mailOptions = {
-            from: '"ImperioMate" <' + process.env.EMAIL_USER + '>', // Remitente
+            from: '"ApoloMate" <' + process.env.EMAIL_USER + '>', // Remitente
             to: emailCliente, // Destinatario
             subject: '✅ ¡Pago Recibido! Tu pedido está en marcha', // Asunto
             
             // Cuerpo del correo en HTML (Diseño elegante)
             html: `
                 <div style="font-family: Arial, sans-serif; max-width: 600px; margin: 0 auto; background-color: #121212; color: #fff; padding: 30px; border-radius: 10px; border: 1px solid #c5a059;">
-                    <h1 style="color: #c5a059; text-align: center; font-size: 28px;">IMPERIO MATE</h1>
+                    <h1 style="color: #c5a059; text-align: center; font-size: 28px;">Apolo Mate</h1>
                     <h2 style="color: #4caf50;">¡Hola ${nombreCliente}! 👋</h2>
                     
                     <p style="font-size: 16px; color: #e0e0e0; line-height: 1.5;">
@@ -38,7 +38,7 @@ const enviarCorreoCompra = async (emailCliente, nombreCliente, idPedido, total) 
                     </div>
                     
                     <p style="text-align: center; color: #888; font-size: 12px; margin-top: 40px; border-top: 1px solid #333; padding-top: 20px;">
-                        Gracias por confiar en ImperioMate.<br>San Juan, Argentina. 🧉
+                        Gracias por confiar en ApoloMate.<br>San Juan, Argentina. 🧉
                     </p>
                 </div>
             `
